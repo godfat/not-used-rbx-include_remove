6c2e7dc1f765d936b1c1c0f3618a4ce7bcf39be3
godfat <godfat@godfat.org>
2008-11-02 00:44:40 +0800

    added include_remove, which contains extend_remove too.

---
+++ include_remove.rb
@@ -0,0 +1,27 @@
+class Module
+
+  def include_remove mod
+    raise TypeError.new('expected Module') unless mod.instance_of?(Module)
+    raise ArgumentError.new('prohibited removing Kernel') if mod == Kernel
+
+    target, prev = superclass_chain.inject(self){ |prev, ancestor|
+      if ancestor.kind_of?(IncludedModule) && ancestor.module == mod
+        break [ancestor, prev]
+      else
+        ancestor
+      end
+    }
+
+    if prev
+      prev.superclass = target.superclass
+      true
+    else
+      false
+    end
+  end
+
+  def extend_remove mod
+    metaclass.include_remove(mod)
+  end
+
+end
