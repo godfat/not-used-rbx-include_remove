27efc492a12a6be9f337b1048fce0d0ed924a15c
godfat <godfat@godfat.org>
2008-11-02 02:36:14 +0800

    put things into module IncludeRemove, then IncludeRemove could suicide.
      * do things only if we are on rubinius.
      * some local variables renaming.
      * added .gitignore to ignore *.rbc

--- include_remove.rb
+++ include_remove.rb
@@ -1,27 +1,33 @@
-class Module
 
-  def include_remove mod
-    raise TypeError.new('expected Module') unless mod.instance_of?(Module)
-    raise ArgumentError.new('prohibited from removing Kernel') if mod == Kernel
+if defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+  module IncludeRemove
 
-    target, prev = superclass_chain.inject(self){ |prev, ancestor|
-      if ancestor.kind_of?(IncludedModule) && ancestor.module == mod
-        break [ancestor, prev]
+    def include_remove mod
+      raise TypeError.new('expected Module') unless mod.instance_of?(Module)
+      raise ArgumentError.new('prohibited from removing Kernel') if mod == Kernel
+
+      target, last = superclass_chain.inject(self){ |last, target|
+        if target.kind_of?(IncludedModule) && target.module == mod
+          break [target, last]
+        else
+          target
+        end
+      }
+
+      if last
+        last.superclass = target.superclass
+        true
       else
-        ancestor
+        false
       end
-    }
-
-    if prev
-      prev.superclass = target.superclass
-      true
-    else
-      false
     end
+
+    def extend_remove mod
+      metaclass.include_remove(mod)
+    end
+
   end
 
-  def extend_remove mod
-    metaclass.include_remove(mod)
-  end
+  Module.include(IncludeRemove)
 
 end
